845def3cfc2199283c23748314ccd5a0d668fb52
SerhiiRodin <rodinserj@ukr.net>
2023-02-03 22:39:19 +0200

    add task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormeSubmit);
+
+function onFormeSubmit(event) {
+  event.preventDefault();
+
+  let delay = +form.elements.delay.value;
+  const { step, amount } = form.elements;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(value => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${value.i} in ${value.delay}ms`
+        );
+      })
+      .catch(value => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${value.i}} in ${value.delay}ms`
+        );
+      });
+
+    delay += +step.value;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
